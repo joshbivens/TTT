a3f73a7cb938f5ccef6e3a9ed390e2d103eb7d3c
Josh Bivens <hotbioform@gmail.com>
2017-01-03 21:58:24 -0700

    Reorder the program/add side-setting function

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,25 @@
 const game = document.querySelector('#game');
 const spacesData = [[], [], [], [], [], [], [], [], []];
 
+// Make the playing field
 const grid = spacesData.map((space, index) => `<div data-index="${index}" class="space"></div>`);
 game.innerHTML = grid;
-
 const spaces = [...document.querySelectorAll('.space')];
 
 // Event Listeners
-// spaces.map(space => space.addEventListener('click', setSide));
+spaces.map(space => space.addEventListener('click', setSide));
 // spaces.map(space => space.addEventListener('click', playGame));
+
+function setSide() {
+  this.innerHTML = "X";
+  console.log(this)
+}
+
+// For later: think about the relationship between the spacesData array and the spaces that came from it.
+// How will we, when you click on a space, add a 'true' or 'X' to the corresponding subarray in spacesData?
+
+function checkSpaces() {
+  for(let i = 0; i < spacesData.length; i++) {
+    // ??? We need to figure out setSide first!
+  }
+}
