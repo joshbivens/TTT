23db2aa74115dda0725e27981613de24f51d773d
Josh Bivens <hotbioform@gmail.com>
2017-01-04 13:28:13 -0700

    Make checkSpace and computerMove functions

--- tictactoe.js
+++ tictactoe.js
@@ -8,7 +8,7 @@ const spaces = [...document.querySelectorAll('.space')];
 
 // Event Listeners
 spaces.map(space => space.addEventListener('click', setSide));
-// spaces.map(space => space.addEventListener('click', playGame));
+spaces.map(space => space.addEventListener('click', computerMove));
 
 function setSide() {
   this.innerHTML = "X";
@@ -18,8 +18,46 @@ function setSide() {
   
 }
 
-// function checkSpaces() {
-//   for(let i = 0; i < spacesData.length; i++) {
-    
-//   }
-// }
+function checkSpaces() {
+  let temp = 0;
+  for(let i = 0; i < spacesData.length; i++) {
+    // Run through each space
+    if(spacesData[i] == "X") {
+      temp += parseInt(spaces[i].dataset.index)
+    }
+  }
+  console.log(temp);
+  return temp;
+}
+
+// Seems to be working for a few case, but feels very inefficient
+function computerMove() {
+  let userMove = checkSpaces();
+  switch (userMove) {
+    case 17:
+      setTimeout(() => spaces[8].innerHTML = "O", 700);
+      break;
+    case 14:
+      setTimeout(() => spaces[5].innerHTML = "O", 700);
+      break;
+    case 13:
+    case 8:
+      setTimeout(() => spaces[4].innerHTML = "O", 700);
+      break;
+    case 10:
+      setTimeout(() => spaces[2].innerHTML = "O", 700);
+      break;
+    case 6:
+    case 3:
+      setTimeout(() => spaces[6].innerHTML = "O", 700);
+      break;
+    case 1:
+      setTimeout(() => spaces[2].innerHTML = "O", 700);
+      break;
+    case 0:
+      setTimeout(() => spaces[8].innerHTML = "O", 700);
+      break;
+    default:
+      alert("Make a valid move!");
+  }
+}
