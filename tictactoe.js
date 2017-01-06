a961360af050496e779cf4d3eec0025afe4296ba
Josh Bivens <hotbioform@gmail.com>
2017-01-05 22:28:17 -0700

    Try different approaches to checking for win

--- tictactoe.js
+++ tictactoe.js
@@ -18,46 +18,56 @@ function setSide() {
   
 }
 
+// We want the function to check each subarray and if it contains an "X", check the next spaces that would contain an "X" for a win.
+
 function checkSpaces() {
   let temp = 0;
   for(let i = 0; i < spacesData.length; i++) {
-    // Run through each space
     if(spacesData[i] == "X") {
-      temp += parseInt(spaces[i].dataset.index)
+      temp += parseInt(spaces[i].dataset.index) + 1;
     }
   }
   console.log(temp);
   return temp;
 }
 
-// Seems to be working for a few case, but feels very inefficient
-function computerMove() {
-  let userMove = checkSpaces();
-  switch (userMove) {
-    case 17:
-      setTimeout(() => spaces[8].innerHTML = "O", 700);
-      break;
-    case 14:
-      setTimeout(() => spaces[5].innerHTML = "O", 700);
-      break;
-    case 13:
-    case 8:
-      setTimeout(() => spaces[4].innerHTML = "O", 700);
-      break;
-    case 10:
-      setTimeout(() => spaces[2].innerHTML = "O", 700);
-      break;
-    case 6:
-    case 3:
-      setTimeout(() => spaces[6].innerHTML = "O", 700);
-      break;
-    case 1:
-      setTimeout(() => spaces[2].innerHTML = "O", 700);
-      break;
-    case 0:
-      setTimeout(() => spaces[8].innerHTML = "O", 700);
-      break;
-    default:
-      alert("Make a valid move!");
-  }
-}
+
+// =========================
+// ========= NOTES =========
+// =========================
+// So there's there's vertical, horizontal, and diagonal wins, and 2 ways to do it: consecutively and alternately.
+
+
+// function checkForWin() {
+//   let curr = checkSpaces();
+//   if(curr.indexOf(0) > -1 /*|| curr == [1] || curr == [3] || curr == [4]*/) {
+//     if(curr.indexOf(1) > -1) {
+//       setTimeout(() => spaces[2].innerHTML = "O", 700);
+//     }
+//   }
+// }
+
+// function computerMove() {
+//   let userMove = checkSpaces();
+//   switch (userMove) {
+//     case 20:
+//     case 19:
+//       setTimeout(() => spaces[4].innerHTML = "O", 700);
+//     case 14:
+//     case 13:
+//       setTimeout(() => spaces[2].innerHTML = "O", 700);
+//     case 12:
+//       setTimeout(() => spaces[1].innerHTML = "O", 700);
+//     case 6:
+//     case 5:
+//     case 4:
+//       setTimeout(() => spaces[6].innerHTML = "O", 700);
+//     case 3:
+//     case 2:
+//     case 1:
+//       setTimeout(() => spaces[8].innerHTML = "O", 700);
+//       break;
+//     default:
+//       console.log("Make a valid move!");
+//   }
+// }
